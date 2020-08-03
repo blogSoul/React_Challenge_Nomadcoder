dc93e8819ef239099e157058a43c77c7ecaf2829
blo9040 <blo9040@gmail.com>
2020-08-03 18:12:03 +0900

    add chapter3.3

--- nomflix/src/Components/App.js
+++ nomflix/src/Components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 //import { HashRouter as Router, Route } from "react-router-dom";
 import Router from "Components/Router"
+import GlobalStyles from 'Components/GlobalStyles';
 
 class App extends Component {
   render() {
     return (
       <>
         <Router />
+        <GlobalStyles />
       </>
     );
   }
