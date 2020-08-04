f71e895fdbe9e2c5652419b7b91fcf5f8d02ac92
blo9040 <blo9040@gmail.com>
2020-08-05 02:09:12 +0900

    add api

--- nomflix/README.md
+++ nomflix/README.md
@@ -11,5 +11,8 @@
 
 - [] Now playing(moive)
 - [] Top Rated(TV, Movie)
-- [] Popular(TV, Movie)
+- [] Popular(TV, X)
 - [] Airing Today(TV)
+- [] TV detail
+- [] Movies detail
+- [] search Movies, TV
--- nomflix/src/api.js
+++ nomflix/src/api.js
@@ -1,13 +1,41 @@
 import axios from "axios";
  
 const api = axios.create({
-    baseURL: "https://api.themoviedb.org/3/",
+    baseURL: 'https://api.themoviedb.org/3/',
     params: {
-        api_key: process.env.MOIVE_API_KEY,
+        api_key: process.env.REACT_APP_API_KEY,
         language: "en-US"
     }
 });
 
-api.get("tv/popular");
+export const moviesApi = {
+    nowPlaying: () => api.get("moive/now_playing"),
+    upcoming: () => api.get("movie/upcoming"),
+    popular: () => api.get("movie/popular"),
+    movieDetail: id => api.get(`movie/${id}`, {
+        params: {
+            append_to_respone: "videos"
+        }
+    }),
+    search: (term) => api.get("search/movie", {
+        params: {
+            query: encodeURIComponent(term)
+        }
+    })
+};
 
-export default api;
+export const tvApi = {
+    topRated: () => api.get("tv/top_rated"),
+    popular: () => api.get("tv/popular"),
+    airingToday: () => api.get("tv/airing_today"),
+    showDetail: id => api.get(`tv/${id}`, {
+        params: {
+            append_to_respone: "videos"
+        }
+    }),
+    search: (term) => api.get("search/tv", {
+        params: {
+            query: encodeURIComponent(term)
+        }
+    })
+};
