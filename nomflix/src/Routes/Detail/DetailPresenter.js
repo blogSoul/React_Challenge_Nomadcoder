62fa64684c01e12fc70d9aeb7badc1a0d33af659
blogSoul <blo9040@gmail.com>
2020-08-26 23:23:14 +0900

    add poster component context

--- nomflix/src/Routes/Detail/DetailPresenter.js
+++ nomflix/src/Routes/Detail/DetailPresenter.js
@@ -2,6 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
+const Container = styled.div`
+    height: calc(100vh - 50px);
+    width: 100%;
+`;
+
+const Backdrop = styled.div``;
+
 const DetailPresenter = ({ result, loading, error }) => null;
 
 DetailPresenter.propTypes = {
