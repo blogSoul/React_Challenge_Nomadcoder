ef6331ed1dbc5eb49d8fbfdc938de90e19f619f8
blogSoul <blo9040@gmail.com>
2020-08-27 00:40:12 +0900

    add Detail component

--- nomflix/src/Components/Header.js
+++ nomflix/src/Components/Header.js
@@ -12,6 +12,7 @@ const Header = styled.header`
   display:flex;
   align-items:center;
   background-color:rgba(20, 20, 20, 0.8);
+  z-index: 10;
   box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
 `;
 
@@ -20,11 +21,11 @@ const List = styled.ul`
 `;
 
 const Item = styled.li`
-  width: 50px;
+  width: 80px;
   height: 50px;
   text-align: center;
-  border-bottom: 5px solid ${props => (props.current ? "#3498db" : "transparent")};
-  transition: border-bottom .5s ease-in-out;
+  border-bottom: 3px solid ${props => (props.current ? "#3498db" : "transparent")};
+  transition: border-bottom 0.5s ease-in-out;
 `;
 
 const SLink = styled(Link)`
